4aeee6a87e00eb5f5bc5767b3f93d96f36ea11f2
zhangjp <zjp1029@aliyun.com>
2018-12-14 11:03:40 +0800

    列表页面基本完成

---
+++ src/pages/city/City.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <city-header></city-header>
+    <city-search :cities="cities"></city-search>
+    <city-list :letter="letter" :cities="cities" :hot="hotCities"></city-list>
+    <city-alphablet :cities="cities" @change="handleLetterChange"></city-alphablet>
+  </div>
+</template>
+
+<script>
+import CityHeader from './components/Header'
+import CitySearch from './components/Serarch'
+import CityList from './components/List'
+import CityAlphablet from './components/Alphabet'
+import axios from 'axios'
+export default {
+  name: 'City',
+  components: {
+    CitySearch,
+    CityHeader,
+    CityList,
+    CityAlphablet
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: [],
+      letter: ''
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
+    }
+  },
+  mounted () {
+    this.getCityInfo()
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市景点/游玩主题
     </div>
-    <div class="header-right">
-      {{city}}
-      <span class="iconfont arrow-icon">&#xe6aa;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{city}}
+        <span class="iconfont arrow-icon">&#xe6aa;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -27,8 +29,8 @@ export default {
   @import "~styles/varibles.styl"
   .header
     display: flex
-    height: .86rem
-    line-height: .86rem
+    height: $headerHeight
+    line-height: $headerHeight
     color: #ffffff
     background-color: $bgColor
     .header-left
@@ -51,6 +53,7 @@ export default {
       width: 1.24rem
       float: right
       text-align: center
+      color: #ffffff
       .arrow-icon
         margin-left: -.04rem
         .24rem
